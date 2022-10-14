8dc6388b13b37180290a671a82563fbcd3ce46a2
Anton <antonvzm@gmail.com>
2022-10-14 13:43:56 +0300

    add Task00, код для нахождения квадрата числа

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿// Напишите программу, которая на вход
+// 1.принимает число и выдаёт его квадрат (число
+// 2.умноженное на само себя).
+// Например:
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+Console.WriteLine("Этот код находит квадрат целого числа");
+Console.Write("Введите целое число:  ");
+int number = Convert.ToInt32 (Console.ReadLine());
+
+//string str = Console.ReadLine();
+//int num = Convert.ToInt32(str);
+
+int square= number * number;
+Console.WriteLine ($"Квадрат числа {number} = {square}");
