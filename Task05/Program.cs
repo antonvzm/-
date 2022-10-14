35ec0a50f0dae48479b683e89b91db014831cb0d
Anton <antonvzm@gmail.com>
2022-10-14 17:06:59 +0300

    add Task05 код для нахождения наибольшего числа

---
+++ Task05/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 2: Напишите программу, которая на вход
+//принимает два числа и выдаёт, какое число большее, а
+//какое меньшее.
+
+Console.Write("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > numberB)
+{
+Console.WriteLine($"Большее число {numberA} ");
+}
+else if (numberB > numberA)
+{
+Console.WriteLine($"Большее число {numberB} ");
+}
