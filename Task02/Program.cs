0561399566d49627cd06fa69e0b65d6fdf9868f9
Anton <antonvzm@gmail.com>
2022-10-14 15:43:47 +0300

    add Task02, код для определения дня недели

---
+++ Task02/Program.cs
@@ -0,0 +1,55 @@
+﻿//Напишите программу, которая будет выдавать
+//название дня недели по заданному номеру.
+//3 -> Среда
+//5 -> Пятница
+
+Console.Write("Введите дату:" );
+    int data = Convert.ToInt32 (Console.ReadLine());
+       
+    if (data == 1)
+    
+    {
+Console.WriteLine("Понедельник");
+    }
+    
+    if (data == 2)
+
+    {
+        Console.WriteLine("Вторник");
+    }
+    
+      if (data == 3)
+
+    {
+        Console.WriteLine("Среда");
+    }
+
+        if (data == 4)
+
+    {
+        Console.WriteLine("Четверг");
+    }
+
+        if (data == 5)
+
+    {
+        Console.WriteLine("Пятница");
+    }
+
+        if (data == 6)
+
+    {
+        Console.WriteLine("Суббота");
+    }
+
+        if (data == 7)
+
+    {
+        Console.WriteLine("Воскресенье");
+    }
+
+        else
+
+    {
+        Console.WriteLine("Введите число от 1 до 7");
+    }
