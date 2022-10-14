55b42f18056a92d11c7cd0041264948d6107bb67
Anton <antonvzm@gmail.com>
2022-10-14 19:05:35 +0300

    add Task08, нахождение четных чисел в заданном диапазоне

---
+++ Task08/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая на вход
+//принимает число (N), а на выходе показывает все чётные
+//числа от 1 до N.
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 0)
+{
+int count = 0;
+while (count <= number)
+{
+    int count2 = count % 2;
+    if (count2 == 0 )
+    {
+        Console.Write($"{count}");
+    }
+    count++;
+}
+}
+else Console.WriteLine("Введите положительное число");
