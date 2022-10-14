5a38a9e44910e90ef6b342748cd1654a76e5673d
Anton <antonvzm@gmail.com>
2022-10-14 15:17:52 +0300

    add Task01, код для проверки квадрата числа

---
+++ Task01/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите цельное число: ");
+    int number = Convert.ToInt32 (Console.ReadLine());
+Console.Write("Введите квадрат этого числа: ");
+    int square = Convert.ToInt32 (Console.ReadLine());
+
+    if (number == square/number )
+{
+Console.WriteLine($"Квадрат {number} это {square}");
+
+}
+
+    else
+{
+Console.WriteLine($"Квадрат {number} не равен {square}");
+
+}
