6282d12028177e9ed8417dd4086046fa52411ccc
Anton <antonvzm@gmail.com>
2022-10-19 00:52:21 +0300

    add Task14

---
+++ Task14/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на
+//вход трёхзначное число и на выходе показывает вторую
+//цифру этого числа.
+
+
+Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int num1 = number / 100;
+int num2 = (number - (num1 * 100))/10;
+if (number < 100)
+Console.WriteLine("Число не трехзначное");
+else
+Console.WriteLine(num2);
