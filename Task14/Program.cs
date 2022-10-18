1c65d47f91f2198afb79337c4fd97b4ef00663da
Anton <antonvzm@gmail.com>
2022-10-19 01:31:30 +0300

    edit Task14

--- Task14/Program.cs
+++ Task14/Program.cs
@@ -7,7 +7,7 @@ Console.WriteLine("Введите трехзначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 int num1 = number / 100;
 int num2 = (number - (num1 * 100))/10;
-if (number < 100)
+if (number < 100 || number > 999)
 Console.WriteLine("Число не трехзначное");
 else
 Console.WriteLine(num2);
