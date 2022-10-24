ba6e9776f3ae4e3192b150ae10c8c0dea0ccbb26
Anton <antonvzm@gmail.com>
2022-10-24 19:18:35 +0300

    add Task23

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -6,3 +6,24 @@
 // 3 | 27
 // 4 | 64
 // 5 | 125
+
+Console.WriteLine("Введите число");
+Console.Write("N = ");
+int N = Convert.ToInt32(Console.ReadLine());
+void qube(int n)
+{
+    if (n > 0)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            double sqI = Math.Pow(i, 3);
+            Console.WriteLine($"{i} | {sqI}");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Значение должно быть < 0");
+    }
+    }
+Console.WriteLine($"{N} -> ");
+qube(N);
