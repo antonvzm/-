694ff282440efcd09ec6c614ead5a6948b598f01
Anton <antonvzm@gmail.com>
2022-10-24 15:39:58 +0300

    add Task20

---
+++ Task20/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 22: Напишите программу, которая
+// принимает на вход число (N) и выдаёт таблицу
+//  квадратов чисел от 1 до N.
+
+Console.WriteLine("Введите число");
+Console.Write("N = ");
+int N = Convert.ToInt32(Console.ReadLine());
+void Squar(int n)
+{
+    if (n > 0)
+    {
+        for (int i = 0; i <= n; i++)
+        {
+            int sqI = i * i;
+            Console.Write($"{sqI}, ");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Значение должно быть < 0");
+    }
+    }
+
+Squar(N);
