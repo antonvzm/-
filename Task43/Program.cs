ba1e0ea26a83ab44213f41b62415926cd8b6c707
«Anton» <antonvzm@gmail.com>
2022-11-18 21:09:27 +0300

    add Task43,44

---
+++ Task43/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задание
+// Задача 41: Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+
+
+
+
+
+int[] CreateArray(int size)
+{
+    int[] array = new int [size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Введите элемент массива с индексом {i}: ");
+        array[i] = Convert.ToInt32 (Console.ReadLine());
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]},");
+        else Console.Write($"{array[i]}");
+    }
+    Console.Write("]");
+}
+
+int SearchZero(int[] array)
+{
+    int summ = default;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == 0) 
+        summ = summ + 1;
+    }
+    return summ;
+}
+
+Console.Write("Введите колличество элементов массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+    if (size < 1)
+Console.WriteLine("Элементов массива не может быть меньше одного");
+    else
+{
+int[] arr = CreateArray(size);
+PrintArray(arr);
+int sum = SearchZero(arr);
+Console.WriteLine($"->{sum}");
+}
---
+++ Task44/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 43. Напишите программу, которая найдёт точку
+// пересечения двух прямых, заданных уравнениями y = k1 *
+// x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются
+// b1 = 2, k1 = 5, b2 = 4, k2 =
+// 9 -> (-0,5; -0,5)
+
+// double SearchX(double b1, double k1, double b2, double k2)
+// {
+//     double x = (b2 - b1)/(k1-k2);
+//     return x;
+// }
+// double SearchY(double x, double k1, double b1)
+// {
+//      double y = k1 * x + b1;
+//      return y;
+// }
+
+
+Console.WriteLine("Введите b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+// double x = SearchX(b1, b2, k1, k2);
+// double y = SearchY(x, k1, b1);
+
+    double x = (b2 - b1)/(k1-k2);
+    double y = k1 * x + b1;
+   
+
+Console.WriteLine($"b1 = {b1}, {b2} = 2, k1 = {k1}, k2 = {k2} -> ({x} ; {y})");
