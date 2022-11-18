4bd61a545f4b4eacc6c43c5f09d161e592d56808
«Anton» <antonvzm@gmail.com>
2022-11-19 01:58:55 +0300

    add Task45-51

---
+++ Task47/Program.cs
@@ -0,0 +1,57 @@
+﻿// Решение в группах задач:
+// Задача 49: Задайте двумерный массив. Найдите элементы, у
+// которых оба индекса чётные, и замените эти элементы на их
+// квадраты.
+// Например, изначально массив
+//  выглядел вот так:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+
+
+int[,] CreateMatrixRndInt(int rows, int colums, int min, int max)
+{
+    int[,] matrix = new int[rows, colums];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max) + 1;
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5} |");
+            else Console.Write($"{matrix[i, j],5}");
+        }
+        Console.WriteLine(" |");
+    }
+}
+
+int[,] SearchEvenIndicesReplecementSquare(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i+=2)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j+=2)
+        {  
+            matrix[i, j] = matrix[i, j] * matrix[i, j];
+        }
+    }
+    return matrix;
+}
+
+int[,] matrix = CreateMatrixRndInt(5, 5, 0, 9);
+PrintMatrix(matrix);
+Console.WriteLine();
+int[,] newMatrix = SearchEvenIndicesReplecementSquare(matrix);
+PrintMatrix(newMatrix);
---
+++ Task48/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 51: Задайте двумерный массив. Найдите сумму
+// элементов, находящихся на главной диагонали (с индексами
+// (0,0); (1; 1) и т.д.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+
+int[,] CreateMatrixRndInt(int rows, int colums, int min, int max)
+{
+    int[,] matrix = new int[rows, colums];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max) + 1;
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5} |");
+            else Console.Write($"{matrix[i, j],5}");
+        }
+        Console.WriteLine(" |");
+    }
+}
+
+int SearchNumbers(int[,] matrix)
+{
+    int sum = default;
+   for (int i = 0; i < matrix.GetLength(0); i++)
+   {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i == j) 
+            sum = sum + matrix[i,j];
+        }
+   }
+    return sum;
+}
+
+int[,] matrix = CreateMatrixRndInt(5, 7, 0, 9);
+PrintMatrix(matrix);
+Console.WriteLine();
+Console.WriteLine(SearchNumbers(matrix));
